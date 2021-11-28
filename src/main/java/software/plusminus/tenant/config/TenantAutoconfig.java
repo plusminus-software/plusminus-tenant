86131dd84ae072a19774d29a26b18f42e6904720
Taras Shpek <taras.shpek@gmail.com>
2021-11-29 01:37:35 +0200

    fix TenantAutoconfig

--- src/main/java/software/plusminus/tenant/config/TenantAutoconfig.java
+++ src/main/java/software/plusminus/tenant/config/TenantAutoconfig.java
@@ -3,7 +3,7 @@ package software.plusminus.tenant.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan({"software.plusminus.tenant", "software.plusminus.security", "software.plusminus.jwt"})
+@ComponentScan("software.plusminus.tenant")
 @Configuration
 public class TenantAutoconfig {
 }
